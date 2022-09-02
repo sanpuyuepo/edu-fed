7e8cc0fb9897b1f37beec4d19375b6e22392fb46
sanpuyuepo <sanpuyuepo_scu@163.com>
2022-09-02 09:47:56 +0800

    删除模板项目路由, 添加文件说明

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,16 @@ module.exports = {
   env: {
     node: true
   },
+  // 插件：扩展了校验规则
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    'plugin:vue/essential', // eslint-plugin-vue
+    '@vue/standard', // @vue/eslint-config-standard
+    '@vue/typescript/recommended' // @vue/eslint-config-typescript
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
+  // ⾃定义验证规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,5 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+// ts 无法识别 .vue 结尾的文件
+// 声明 所有以 .vue 结尾的文件模块, 其类型都是 vue 构造函数, 进行适配处理

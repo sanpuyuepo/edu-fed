4819626719231f68b9df832f6fa42d1cf93b1e84
sanpuyuepo <sanpuyuepo_scu@163.com>
2022-09-03 21:04:25 +0800

    feat: Handling Token expiration: multiple requests failed & resend

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -19,6 +19,17 @@ const redirectLogin = function () {
   })
 }
 
+const refreshToken = function () {
+  return axios.create()({
+    method: 'POST',
+    url: '/front/user/refresh_token',
+    data: qs.stringify({
+      // refresh_token 只能使用一次
+      refreshtoken: store.state.user.refresh_token
+    })
+  })
+}
+
 // 添加请求拦截器
 request.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么: 改写配置信息实现业务功能的统一处理
@@ -37,6 +48,11 @@ request.interceptors.request.use(function (config) {
 })
 
 // Add a response interceptor
+
+// refresh Token flag
+let isRefreshing = false
+// 存储刷新 token 之前发送失败的请求 401
+let requests: any[] = []
 request.interceptors.response.use(function (response) {
   // 如果是自定义错误状态码, 错误处理在这里
   return response
@@ -53,25 +69,38 @@ request.interceptors.response.use(function (response) {
         return Promise.reject(error)
       }
 
-      // 尝试刷新获取新的 token, 新建一个axios实例处理
-      try {
-        const { data } = await axios.create()({
-          method: 'POST',
-          url: '/front/user/refresh_token',
-          data: qs.stringify({
-            refreshtoken: store.state.user.refresh_token
-          })
+      if (!isRefreshing) {
+        isRefreshing = true
+        // 尝试刷新获取新的 token
+        return refreshToken().then(res => {
+          if (!res.data.success) {
+            throw new Error('刷新 Token 失败')
+          }
+          // 刷新token成功
+          store.commit('setUser', res.data.content)
+          // 执行 requests 中挂起的请求
+          requests.forEach(cb => cb())
+          // 重置 requests 数组
+          requests = []
+          return request(error.config)
+        }).catch(err => {
+          console.log(err)
+          // 清除当前登录用户状态
+          store.commit('setUser', null)
+          redirectLogin()
+          return Promise.reject(error)
+        }).finally(() => {
+          // 重置刷新状态
+          isRefreshing = false
         })
-        // 把成功刷新拿到的新的 access_token, 更新到容器和本地存储中
-        store.commit('setUser', data.content)
-        // 重新发送本次失败的请求
-        return request(error.config)
-      } catch (err) {
-        // 清除当前登录用户状态
-        store.commit('setUser', null)
-        redirectLogin()
-        return Promise.reject(error)
       }
+
+      // ! 刷新状态下挂起请求(通过返回一个pending状态的promise), 放到 requests 数组
+      return new Promise(resolve => {
+        requests.push(() => {
+          resolve(request(error.config))
+        })
+      })
     } else if (status === 403) {
       Message.error('没有权限, 请联系管理员')
     } else if (status === 404) {
@@ -101,12 +130,12 @@ refresh_token: 刷新获取新的 access_token
 
 ? 为什么 access_token 需要过期时间以及为什么过期时间比较短: 安全
 
-方法1:
+方法1: 请求拦截
 在请求发起前拦截请求, 判断 token 是否过期, 如果过期, 则先刷新token后再继续请求
 优点: 在请求前拦截, 少发送一次请求, 省流量
 缺点: 需要后端额外提供一个token过期时间的字段; 使用本地时间判断, 若本地时间被篡改, 特别是本地时间比服务器时间慢时, 拦截会失败
 
-方法2:
+方法2: 响应拦截
 先发起请求, 接口返回过期后,先刷新 token, 再发送一次请求
 优点: 不需额外的 token 过期字段, 不需要判断时间
 缺点: 多消耗一次请求, 耗流量

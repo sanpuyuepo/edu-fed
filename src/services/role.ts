d3d602b768276111d368dfabc7074d0ca398de4f
sanpuyuepo <sanpuyuepo_scu@163.com>
2022-09-06 10:28:40 +0800

    feat: role management: init role service

---
+++ src/services/role.ts
@@ -0,0 +1,39 @@
+/**
+ * 角色管理相关请求模块
+ */
+
+import request from '@/utils/request'
+
+// 获取所有角色
+export const getAllRoles = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/role/all'
+  })
+}
+
+// 条件分页查询
+export const getRolePages = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/getRolePages',
+    data: data
+  })
+}
+
+// 保存或更新角色
+export const createOrUpdateRole = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/saveOrUpdate',
+    data: data
+  })
+}
+
+// 删除资源
+export const deleteRole = (id: number) => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/role/${id}`
+  })
+}

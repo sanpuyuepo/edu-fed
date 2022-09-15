ff9e449b3a36ae5bd42ecdfacd8b959c92381670
sanpuyuepo <sanpuyuepo_scu@163.com>
2022-09-15 11:08:02 +0800

    chore: update eslint typescript rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,13 @@ module.exports = {
   // ⾃定义验证规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
+        requireLast: true
+      }
+    }],
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 }
